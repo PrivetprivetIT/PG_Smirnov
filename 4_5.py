2b8277b5ae645c1497b0fbfb10673ef3ae372c7f
PrivetprivetIT <Smirnovsasha20@gmail.com>
2025-03-16 15:37:34 +0300

    16.03.2025

--- 4_5.py
+++ 4_5.py
@@ -1,17 +1,19 @@
 from all_colors import *
+from pygame.constants import *
 import pygame
 pygame.init()
 
 size = (800, 600)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Моя игра')
-BACKGROUND = YELLOW
-screen.fill(BACKGROUND)
+BACKGROUND = pygame.image.load('resours/sky.jpg')
 
-my_font = pygame.font.SysFont('Arial', 32)
-my_text = my_font.render('Потрачено!', True, (0, 0, 0), (BACKGROUND))
-screen.blit(my_text, (100, 100))
+plane1 = pygame.image.load('resours/plane1.png')
+plane2 = pygame.image.load('resours/plane2.png')
 
+speed = 5
+x, y = 50, 50
+pos = (400, 400)
 FPS = 60
 clock = pygame.time.Clock()
 
@@ -22,6 +24,35 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if pygame.mouse.get_focused():
+            pos = pygame.mouse.get_pos()
+
+
+    keys = pygame.key.get_pressed()
+
+    if keys[K_LEFT]:
+        x -= speed
+    if keys[K_RIGHT]:
+        x += speed
+    if keys[K_UP]:
+        y -= speed
+    if keys[K_DOWN]:
+        y += speed
+
+    if y < 0:
+        y = 0
+    if y > 400:
+        y = 400
+    if x < 0:
+        x = 0
+    if x > 600:
+        x = 600
+
+
+
+    screen.blit(BACKGROUND, (0, 0))
+    screen.blit(plane1, (x, y))
+    screen.blit(plane2, (pos[0] - 112, pos[1] - 80))
 
     pygame.display.flip()
     clock.tick(FPS)
